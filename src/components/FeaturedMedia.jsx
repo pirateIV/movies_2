8cb5932ed13bc0221f0eea7df7a657797069ffa4
pirateIV <benabolade@gmail.com>
2024-05-28 17:04:24 +0100

    fix: style issues

--- src/components/FeaturedMedia.jsx
+++ src/components/FeaturedMedia.jsx
@@ -4,12 +4,12 @@ const FeaturedMedia = ({ featured }) => {
   console.log(featured);
   return (
     <Link to={`movie/${featured?.id}`} className="h-full" id="featured">
-      <div className="featured-container">
-        <div>
+      <div className="featured-container sm:min-h-[400px]">
+        {/* <div>
           <button>
-            <div i-ph-play-circle-light=""></div>
+            <div className="i-ph-play-circle-light"></div>
           </button>
-        </div>
+        </div> */}
 
         <div className="featured-content absolute z-20 top-0 bottom-0 flex flex-col items-start justify-center ">
           <h1>{featured?.title}</h1>
@@ -19,9 +19,9 @@ const FeaturedMedia = ({ featured }) => {
             Watch Trailer
           </button>
         </div>
-        <div className="featured-image absolute w-2/3 inset-y-0 right-0 z-10 h-full">
+        <div className="featured-image static lg:absolute w-full lg:w-2/3 inset-y-0 bottom-0 lg:right-0 z-10 lg:h-full">
           <img
-            className="h-full w-full object-cover"
+            className="h-auto lg:h-full w-full object-cover"
             src={`https://image.tmdb.org/t/p/w780/${featured?.backdrop_path}`}
             alt=""
           />
